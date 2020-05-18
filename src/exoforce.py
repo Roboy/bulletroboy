7e7fb129366fb8d0591e0ceecaafeccac315fd90
Marisa Ripoll <marisaripoll218@gmail.com>
2020-05-18 11:17:45 +0200

    started with task 4

--- src/exoforce.py
+++ src/exoforce.py
@@ -16,7 +16,7 @@ plane = p.loadURDF("plane.urdf")
 
 human = p.loadURDF(os.path.dirname(os.path.realpath(__file__))+"/../models/human.urdf", [0, 0, 1], useFixedBase=1)
 
-hand_link = 25
+lefthand_link = 25
 # position, orientation = p.getBasePositionAndOrientation(human)
 # orientation
 
@@ -28,7 +28,7 @@ print("The number of Joints is: ", joints)
 for i in range(joints):
 	info = p.getJointInfo(human, i)
 	print(info)
-info = p.getJointInfo(human, hand_link)
+info = p.getJointInfo(human, lefthand_link)
 print(info)
 
 # NUMBER OF CONSTRAINTS = 0 (Unless I uncomments createConstraint function)
@@ -40,12 +40,12 @@ for i in range(cons):
 	print(info)
 
 # GET LINK STATES, JOINT STATES
-linkstate = p.getLinkState(human, hand_link)
-print("The link state or link hand_link is: ", linkstate)
+linkstate = p.getLinkState(human, lefthand_link)
+print("The link state or link lefthand_link is: ", linkstate)
 print("\n")
 
-jointinfo = p.getJointInfo(human, hand_link)
-print("The joint info or link hand_link is: ", jointinfo)
+jointinfo = p.getJointInfo(human, lefthand_link)
+print("The joint info or link lefthand_link is: ", jointinfo)
 
 # NUMBER OF BODIES = 2 (0 = plane, 1 = human)
 bodies = p.getNumBodies()
@@ -76,7 +76,7 @@ color = [1, 0, 0]
 16: Right Wrist
 21: Left Shoulder
 22: Left Elbow
-hand_link: Left Wrist
+lefthand_link: Left Wrist
 30: Right Leg (Point where it joins the waist)
 31: Right Knee
 33: Right Foot
@@ -97,12 +97,15 @@ hand_link: Left Wrist
 
 
 # THIS DEBUGLINE IS ATTACHED TO THE WRIST BUT POINTS ARE DEFINED IN LINK FRAME
-hand_pos = p.getLinkState(human, hand_link)[0]
-tendon = p.addUserDebugLine(lineToXYZ=hand_pos,				# starting point = wrist link frame origin
-		   lineFromXYZ=[1,1,1],			# ending point : how to set it to world frame?
+lefthand_pos = p.getLinkState(human, lefthand_link)[0]
+leftHand_tendon1 = p.addUserDebugLine(lineToXYZ=lefthand_pos,	# starting point = wrist link frame origin
+		   lineFromXYZ=[1,1,1],				# ending point : how to set it to world frame?
                    lineColorRGB= [1,0,0],
                    lineWidth=3)
-
+leftHand_tendon2 = p.addUserDebugLine(lineToXYZ=lefthand_pos,	# starting point = wrist link frame origin
+		   lineFromXYZ=[1,1,-1],			# ending point : how to set it to world frame?
+                   lineColorRGB= [1,0,0],
+                   lineWidth=3)
 
 # THE USERDEBUGLINE BELOW HAS BOTH STARTING AND ENDING POINTS FIXED IN THE WORLD FRAME
 """
@@ -115,8 +118,8 @@ p.addUserDebugLine(lineFromXYZ=[0,0.8,1.35],
 """
 p.createConstraint(human,		# ParentBodyUniqueId
 		   22,			# ParentLinkIndex
-		   hand_link,			# childBodyUniqueId
-		   hand_link,			# childLinkIndex
+		   lefthand_link,			# childBodyUniqueId
+		   lefthand_link,			# childLinkIndex
 		   4,			# Join Type: JOINT_GEAR
 		   [0,0,1],		# Joint Axis
 		   [0,0,0],		# ParentFramePosition
@@ -125,35 +128,57 @@ p.createConstraint(human,		# ParentBodyUniqueId
 # THIS IS THE BIGGEST ISSUE, THE FIGURE DOES NOT REACT TO THE FUNCTION APPLYEXTERNALFORCE
 
 # p.applyExternalForce(objectUniqueId=1,
-# 		     linkIndex = hand_link,
+# 		     linkIndex = lefthand_link,
 # 		     forceObj = [100,100,100],
 # 		     posObj=[0,0,0],
 # 		     flags=2)		# 2 Flags: WORLD_FRAME and LINK_FRAME
 
 # THE CODE LINE BELOW RECEIVES THE SAME REACTION AS THE CREATECONSTRAINT FUNCTION:
-# p.setJointMotorControlArray(human, (21,22,hand_link), p.POSITION_CONTROL)
-hand_pos = p.getLinkState(human, hand_link)[0]
-tendon_origin = [hand_pos[0]+0.5, hand_pos[1]+0.5, 3.0]
+# p.setJointMotorControlArray(human, (21,22,lefthand_link), p.POSITION_CONTROL)
+
+lefthand_pos = p.getLinkState(human, lefthand_link)[0]
+
+leftHand_tendon1_origin = [lefthand_pos[0]+0.5, lefthand_pos[1]+0.5, 2.0]
+leftHand_tendon2_origin = [lefthand_pos[0]+0.5, lefthand_pos[1]+0.5, 0.0]
 # motor_force = 50
-forceId = p.addUserDebugParameter("motor_force", 0,200,0)
+forceId1 = p.addUserDebugParameter("leftHand_tendon1", 0,200,0)
+forceId2 = p.addUserDebugParameter("leftHand_tendon2", 0,200,0)
 
 for _ in range(100000000):
 	# import pdb; pdb.set_trace()
-	hand_pos = p.getLinkState(human, hand_link)[0]
-	motor_force = p.readUserDebugParameter(forceId)
+	lefthand_pos = p.getLinkState(human, lefthand_link)[0]
+	motor_force1 = p.readUserDebugParameter(forceId1)
+	motor_force2 = p.readUserDebugParameter(forceId2)
 
-	p.addUserDebugLine(lineToXYZ=hand_pos,		# XXX
-			   			lineFromXYZ=tendon_origin,		# XXX
-	                    lineColorRGB= [0,0,255],
-	                    lineWidth=2,
-					    replaceItemUniqueId=tendon) # XXX
 
-	force_vector = [a_i - b_i for a_i, b_i in zip(tendon_origin,hand_pos)]
-	force_components = [motor_force * x/norm(force_vector) for x in force_vector]
-	p.applyExternalForce(objectUniqueId=human, # XXX
-			     linkIndex = hand_link,
-			     forceObj = force_components, # XXX
-			     posObj=hand_pos,			# XXX
-			     flags=p.WORLD_FRAME )		# XXX
+	p.addUserDebugLine(lineToXYZ=lefthand_pos,		        # XXX
+			   lineFromXYZ=leftHand_tendon1_origin,		# XXX
+	                   lineColorRGB= [0,0,255],
+	                   lineWidth=2,
+			   replaceItemUniqueId=leftHand_tendon1)         # XXX
+
+	p.addUserDebugLine(lineToXYZ=lefthand_pos,		        # XXX
+			   lineFromXYZ=leftHand_tendon2_origin,		# XXX
+	                   lineColorRGB= [0,0,255],
+	                   lineWidth=2,
+			   replaceItemUniqueId=leftHand_tendon2)         # XXX
+
+	force_vector1 = [a_i - b_i for a_i, b_i in zip(leftHand_tendon1_origin,lefthand_pos)]
+	force_components1 = [motor_force1 * x/norm(force_vector1) for x in force_vector1]
+
+	force_vector2 = [a_i - b_i for a_i, b_i in zip(leftHand_tendon2_origin,lefthand_pos)]
+	force_components2 = [motor_force2 * x/norm(force_vector2) for x in force_vector2]
+
+	p.applyExternalForce(objectUniqueId=human,              # XXX
+			     linkIndex = lefthand_link,
+			     forceObj = force_components1,      # XXX
+			     posObj=lefthand_pos,			# XXX
+			     flags=p.WORLD_FRAME)		# XXX
+
+	p.applyExternalForce(objectUniqueId=human,              # XXX
+			     linkIndex = lefthand_link,
+			     forceObj = force_components2,      # XXX
+			     posObj=lefthand_pos,			# XXX
+			     flags=p.WORLD_FRAME)		# XXX
 
 	p.stepSimulation()

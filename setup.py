151222df54b804d54be88c0745d337367782b6b8
NadaChatti <nadachatti95@gmail.com>
2020-06-02 15:44:14 +0200

    changed to pure python package config files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'test_package_python'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nada',
+    maintainer_email='roboy@todo.todo',
+    description='The bulletroboy package',
+    license='TODO',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            bulletroboy=bulletroboy.ros2_roboy:main,
+        ],
+    },
+)

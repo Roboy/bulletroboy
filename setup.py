0f0145174922d4746a5695e751d04ffb3e2f618d
NadaChatti <nadachatti95@gmail.com>
2020-06-02 16:29:14 +0200

    fixed typos added missing files and deleted unused tests

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-package_name = 'test_package_python'
+package_name = 'bulletroboy'
 
 setup(
     name=package_name,
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            bulletroboy=bulletroboy.ros2_roboy:main,
+            'bulletroboy = bulletroboy.ros2_roboy:main',
         ],
     },
 )

1701db76a4541d1b2e1b2f3ce4ed12e00a406f8e
Karim Alweheshy <karim.alweheshy@gmail.com>
2020-06-02 15:51:34 +0200

    move  pub/sub into same  class

--- src/forces_imitator.py
+++ src/forces_imitator.py
@@ -22,93 +22,69 @@ class ForcesImitator():
         self.publisher = ForcesImitatorPublisher(ContactPoints())
         self.contactPoints = ContactPoints()
 
-    def updateJointState(jointState):
-        self.jointState = jointState
-        self.update()
-
-    def updateCollisionPredictionContactPoints(contactPoints):
-        self.contactPoints = contactPoints
-        self.update()
-
-    def updateRobotFeedbackContactPoints(contactPoints):
-        self.contactPoints = contactPoints
-        self.update()
-
-    def update():
+        self.result = ContactPoints()
         
-        
-        
-
-class JointSubscriber(Node):
-
-    def __init__(self, forcesImitator):
-        super().__init__('joint_subscriber')
-        self.forcesImitator = forcesImitator
-        self.subscription = self.create_subscription(
+        self.jointSubscription = self.create_subscription(
             JointState,
             '/roboy/simulation/joint_state',
-            self.listener_callback,
+            self.listener_joints_callback,
             1)
-        self.subscription  # prevent unused variable warning
 
-    def listener_callback(self, msg):
-        self.forcesImitator.updateJointState(msg)
-        self.get_logger().info('I heard: "%s"' % msg.data)
-
-class EnvironmentCollisionSubscriber(Node):
-
-    def __init__(self, forcesImitator):
-        super().__init__('environment_collision_subscriber')
-        self.forcesImitator = forcesImitator
-        self.subscription = self.create_subscription(
+        self.environmentSubscription = self.create_subscription(
             ContactPoints,
             '/roboy/simulation/environment_collision',
-            self.listener_callback,
+            self.listener_environment_callback,
             1)
-        self.subscription  # prevent unused variable warning
 
-    def listener_callback(self, msg):
-        self.forcesImitator.updateCollisionPredictionContactPoints(msg)
-        self.get_logger().info('I heard: "%s"' % msg.data)
-
-class RobotCollisionSubscriber(Node):
-
-    def __init__(self, forcesImitator):
-        super().__init__('robot_collision_subscriber')
-        self.forcesImitator = forcesImitator
-        self.subscription = self.create_subscription(
+        self.robotCollisionSubscription = self.create_subscription(
             ContactPoints,
             '/roboy/simulation/robot_collision_feedback',
-            self.listener_callback,
+            self.listener_robot_callback,
             1)
-        self.subscription  # prevent unused variable warning
 
-    def listener_callback(self, msg):
-        self.forcesImitator.updateRobotFeedbackContactPoints(msg)
+        rclpy.spin(self.jointSubscription)
+        rclpy.spin(self.environmentSubscription)
+        rclpy.spin(self.robotCollisionSubscription)
+
+        # Destroy the node explicitly
+        # (optional - otherwise it will be done automatically
+        # when the garbage collector destroys the node object)
+        self.jointSubscription.destroy_node()
+        self.environmentSubscription.destroy_node()
+        self.robotCollisionSubscription.destory_node()
+
+        rclpy.shutdown()
+
+        self.publisher = self.create_publisher(ContactPoints, '/roboy/kinematic/forces_imitator', 1)
+        timer_period = 0.1 # seconds
+        self.time = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        self.publisher.publish(self.result)
+
+
+    def listener_joints_callback(self, msg):
+        self.jointState = msg
+        self.update()
         self.get_logger().info('I heard: "%s"' % msg.data)
 
+    def listener_environnment_callback(self, msg):
+        self.contactPoints = msg
+        self.update()
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
+    def listener_robot_callback(self, msg):
+        self.contactPoints = msg
+        self.update()
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
+    def update():
+    
 
 def main(args=None):
     rclpy.init(args=args)
 
     forcesImitator = ForcesImitator(JointState())
-    joint_subscriber = JointSubscriber(forcesImitator)
-    robot_feedback_subscriber = RobotCollisionSubscriber(forcesImitator)
-    environment_collision_subscriber = EnvironmentCollisionSubscriber(forcesImitator)
-
-    rclpy.spin(joint_subscriber)
-    rclpy.spin(robot_feedback_subscriber)
-    rclpy.spin(environment_collision_subscriber)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    joint_subscriber.destroy_node()
-    robot_feedback_subscriber.destroy_node()
-    environment_collision_subscriber.destory_node()
-
-    rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()

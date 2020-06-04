7af925edf8d22057f01252f899602ff0a1cda728
Marisa Ripoll <marisaripoll218@gmail.com>
2020-06-04 13:45:49 +0200

    added docstrings, inputs/outputs explanations, changed get_EF to get_EF_id and set default values

--- bulletroboy/exoforce.py
+++ bulletroboy/exoforce.py
@@ -300,14 +300,16 @@ class Movements():
 	def __init__(self, body_id):
 		"""
 		This class defines 2 types of movements:
-		-  Re-definable inverse kinematic ones (one_end_effector(), multiple_end_effectors())
+		-  Re-definable inverse kinematic ones (one_end_effector(), two_end_effectors())
 		-  Harcoded simple ones (simple_move() --> 4 predefined movements)
 		"""
+
 		self.body_id = body_id
 		self.op = Operator(self.body_id)
 
 
-	def get_EF(self, link_name):
+	def get_EF_id(self, link_name):
+		# Gets the id of the endEffector and the list of free revolute joints 
                 freeJoints = []
                 numJoints = p.getNumJoints(self.body_id)
 
@@ -322,20 +324,52 @@ class Movements():
 
 
 	def apply_chest_and_neck_constraints(self):
+		# The inverse kinematics functions use the spine link as often as the other arm links.
+		# However, ergonomically, humans dont move the spine that much. Constraining the spine
+		# and neck recreates a more human-like movement in certain cases.
+
+		# Constraint on the Chest
 		p.createConstraint(self.body_id, self.op.get_link_index('human/spine_1'), -1,
 				      self.op.get_link_index('human/spine_2'), p.JOINT_FIXED,
 				      [0, 0, 0], [0, 0, 0], [0, 0, 1])
 
+		# Constraint on the Neck
 		p.createConstraint(self.body_id, self.op.get_link_index('human/neck'), -1, 					      self.op.get_link_index('human/spine_2'), p.JOINT_FIXED,
 				      [0, 0, 0], [0, 0, 0], [0, 0, 1])
 
 
-	def one_end_effector(self, link_name, pos, maxIter, chest_constraint):
+	def one_end_effector(self, link_name, pos, maxIter = 100, chest_constraint = True):
+		"""
+		Uses Inverse Kinematics to compute the JointPoses for a given End-Effector position/trajectory.
+		"""
+		
+		#    INPUTS:
+		#	  - link_name:  set it to = b'human/left_hand' to use the left hand as the end_effector or
+		#		        set it to = b'human/right_hand' to use the right hand as the end_effector.
+		#
+		#	  - pos (vec3): x,y,z coordinates of the point that the end_effector is to be positioned at.
+		#			If the movement is to be dynamic, write the xyz vector in terms of t.
+		#
+		#	  - maxIter:	maximum number of iterations for the inverse kinematic computation.
+		#
+		#	  - chest_constraint: (flag) chest and neck constraints for a stable and realistic movement.
+		#
+		#
+		#    OUTPUT:
+		#	  - error: if an error occurs during the inverse kinematic computation or the joint resetting,
+		#	           this value is set to true. (This variable can be used in a while loop to stop
+		#                  implementing the faulty function without using sys.exit())
+		
+		
 		error = False
-		endEffectorId, freeJoints = self.get_EF(link_name)
+		iter = 0
+		endEffectorId, freeJoints = self.get_EF_id(link_name)
+		
+		# Chest and Neck constraints
 		if chest_constraint == True:
 		    self.apply_chest_and_neck_constraints()
-		iter = 0
+		
+		# Try to move the figure. If error occurs, print Warning.
 		try:
 		    while(iter <= maxIter):
 		        jointPoses = p.calculateInverseKinematics(self.body_id, endEffectorId, pos)
@@ -350,16 +384,38 @@ class Movements():
 		return error
 
 
-	def two_end_effectors(self, positions, maxIter, chest_constraint):
+	def two_end_effectors(self, positions, maxIter = 100, chest_constraint = True):
+		"""
+		Uses Inverse Kinematics to compute the JointPoses for the position/trajectory of both hands.
+		"""
+		
+		#    INPUTS:
+		#	  - pos: list of 2 vec3s. pos[0] = desired x,y,z position/trajectory of the left hand.
+		#				  pos[1] = desired x,y,z position/trajectory of the right hand.
+		#
+		#	  - maxIter: maximum number of iterations for the inverse kinematic computation.
+		#
+		#	  - chest_constraint: (flag) chest and neck constraints for a stable and realistic movement.
+		#
+		#
+		#    OUTPUT:
+		#	  - error: if an error occurs during the inverse kinematic computation or the joint resetting,
+		#	           this value is set to true. (This variable can be used in a while loop to stop
+		#                  implementing the faulty function without using sys.exit())
+
 		error = False
-		_, freeJoints = self.get_EF(b'human/left_hand')
+		iter = 0
+
+		# Gets the hand link ids and the free revolute joints
+		_, freeJoints = self.get_EF_id(b'human/left_hand')
 		endEffectorIds = [self.op.get_link_index('human/left_hand'),
                                   self.op.get_link_index('human/right_hand')]
 		
+		# Chest and Neck constraints
 		if chest_constraint == True:
 		    self.apply_chest_and_neck_constraints()
-		iter = 0
 
+		# Try to move the figure. If error occurs, print Warning.
 		try:
 		    while(iter <= maxIter):
 		        jointPoses = p.calculateInverseKinematics2(self.body_id, endEffectorIds, positions)
@@ -376,8 +432,23 @@ class Movements():
 
 
 	def simple_move(self, case):
+		"""
+		Defines a series of hardcoded movements for a direct use. (Movements Library)
+		"""
+		
+		#    INPUTS:
+		#	  - case: names the movement to be applied.
+		#				  
+		#
+		#    OUTPUT:
+		#	  - error: if a non-existent case is used as input, this value is set to true. (This 
+		#		   variable can be used to stop a while loop).
+
+
 		error = False
 		t = time.time()
+
+		# Get link Ids
 		spine_link = self.op.get_link_index('human/spine_2')
 		spine_side_link = self.op.get_link_index('human/spine')
 		left_shoulder_1 = self.op.get_link_index('human/left_shoulder_1')
@@ -387,6 +458,8 @@ class Movements():
 		left_elbow_link = self.op.get_link_index('human/left_elbow')
 		right_elbow_link = self.op.get_link_index('human/right_elbow')
 
+		# Use input to move the figure with the chosen movement.
+		# If a non-existent case is used as input, print warning.
 		if case == 'spine_swing':
                     p.resetJointState(self.body_id, spine_link, math.sin(t))
 

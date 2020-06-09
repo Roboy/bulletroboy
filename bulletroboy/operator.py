55524d534d668a4c5fc414293a629acf3cdb279c
MarisaRipoll <47863300+MarisaRipoll@users.noreply.github.com>
2020-06-09 16:01:47 +0200

    Update bulletroboy/operator.py
    Co-authored-by: Alona Kharchenko <aliona.kharchenko@gmail.com>

--- bulletroboy/operator.py
+++ bulletroboy/operator.py
@@ -43,18 +43,15 @@ class Operator():
 		self.movements.simple_move(case)
 
 	def publish_state(self, publisher, msg):
-		ef_strings = ['human/left_hand', 'human/right_hand']
-		ef = [self.get_link_index(ef_strings[0]), self.get_link_index(ef_strings[1])]
-		endEffectorInfo = [p.getLinkState(self.body_id, ef[i])[:2] for i in range(2)]
-		left_hand_info = p.getLinkState(self.body_id, ef[0])[:2]
-		right_hand_info = p.getLinkState(self.body_id, ef[1])[:2]
+ef_names = ['human/left_hand', 'human/right_hand']
 
-
-		for endEffector in endEffectorInfo:
-			for i in range(2):
-				msg.header.frame_id = ef_strings[i]
-
-				msg.pose.position.x = endEffectorInfo[i][0][0]
+		for ef in ef_names:
+		   ef_id = self.get_link_index(ef)
+		   link_info = p.getLinkState(self.body_id, ef_id)[:2]
+		   link_pos = link_info[0]
+		   link_orn = link_info[1]
+		   msg.header.frame_id = ef
+		   msg.pose.position.x = link.....
 				msg.pose.position.y = endEffectorInfo[i][0][1]
 				msg.pose.position.z = endEffectorInfo[i][0][2]
 
@@ -227,5 +224,4 @@ class Movements():
 
 		elif case == 4:
                     p.resetJointState(self.body_id, spine_side_link, math.sin(t))
-
 

3e91803d84ec643a55864ea4686a60273ec18499
Karim Alweheshy <karim.alweheshy@gmail.com>
2020-06-17 21:19:54 +0200

    Added yaml dict to map roboy links to human links

---
+++ bulletroboy/links_mapper.py
@@ -0,0 +1,19 @@
+import pybullet as p
+import yaml
+
+class LinksMapper(object):
+    def __init__(self, humandURDFFileName, roboyURDFFileName):
+        """
+        This class maps the ids of human links to robot links and vice versa
+        """
+        super().__init__()
+
+        with open('../resources/roboy_to_human_linknam_map.yaml') as f:
+            linkNameMaps = yaml.safe_load(f)
+            self.roboyToHumanLinkNameMap = linkNameMaps.get("roboyToHumanLinkNameMap")
+
+    def humanLinkName(self, robotLinkName):
+        return None
+
+    def robotLinkId(self, humanLinkIndex):
+        return None

bdaeeddeb1da83360d86f82c7ec66fa14ec916f0
MichaelHeiss <michl.heiss@tum.de>
2020-06-22 14:07:30 +0200

    first optimization

--- bulletroboy/optimization.py
+++ bulletroboy/optimization.py
@@ -1,22 +1,25 @@
 import numpy as np
 import os
 from scipy.optimize import minimize
+from utils import CageConfiguration
 import time
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-from utils import CageConfiguration
+MIN_FORCE = 0.01
 
-MIN_FORCE = 0.1
+def equalize(forces):
+    mean = np.mean(forces)
+    distances = forces - mean
+    loss = np.sqrt(np.sum(np.square(distances)))
+    return loss
 
-def force_decomposition(force_vector, force_point, tendons):
+def force_decomposition(force_vector, force_point, tendons, attachment_points):
+    #start = time.time()
     force_direction = force_vector / np.linalg.norm(force_vector)
-
     max_forces = [int(tendons[id]['max_force']) for id in tendons]
-    attachment_points = [tendons[id]['point'] for id in tendons]
-    tendon_vectors = attachment_points - force_point
+    tendon_vectors = (attachment_points - np.array([force_point]).T).T
     tendon_directions = tendon_vectors / np.linalg.norm(tendon_vectors, axis=1, keepdims=True)
-
-    # print("\n\n\nmost important:\n", force_point, "\n\n\n")
 
     # projecting vector directions to force direction, only tendons with positive values, will be part of the decomposition
     projections = tendon_directions.dot(force_direction)
@@ -26,123 +29,198 @@ def force_decomposition(force_vector, force_point, tendons):
     forces = None
     if len(active_tendons) > 0:
         initial_forces = np.random.rand(len(active_tendons)) * np.mean(force_vector)
-        constraints = [{'type':'eq', 'fun': lambda x: np.sum(active_tendons.T * x, axis=1) - force_vector},
-                    {'type':'ineq', 'fun': lambda x: active_max_forces - x}]
-        solution = minimize(lambda x: np.sqrt(np.sum(np.square(x))),
-                                initial_forces,
-                                constraints=constraints, options={'maxiter': 200}
-                                )
+        constraints = [{'type':'eq', 'fun': lambda x: np.sum(active_tendons.T * np.array([x]), axis=1) - force_vector},
+                       {'type':'ineq', 'fun': lambda x: active_max_forces - x},
+                       {'type':'ineq', 'fun': lambda x: x}]
+        solution = minimize(equalize, initial_forces, constraints=constraints, options={'maxiter': 200})
 
         if solution.success:
             forces = {id: force for id, force in zip(tendons, solution.x) if force > MIN_FORCE}
-    
-    # if not forces:
-    #     print(f"WARNING: Cannot decompose the forces: [ {solution.message} ]")
-        
+    # else:
+    #     # print(f"WARNING: Cannot decompose the forces: [ {solution.message} ]")
+    #     forces = None
+
+    #print(f"Time: {time.time() - start} s")
     return forces
 
-def generate_workspace(x, y, z, end_efector, tendon_groups, force_vector=np.array([100, 0, 0])):
-    print(f"Generating workspace for [{end_efector}] \twith force {force_vector}...")
-    start = time.time()
+######   GETTING TENDONS FROM CAGE CONFIGURATION FILE   ######
+cage_conf_file_path = os.path.dirname(os.path.realpath(__file__)) + "/cageConfiguration.xml"
+cage_conf = CageConfiguration(cage_conf_file_path)
 
-    tendons = tendon_groups[end_efector]
+tendon_groups = {}
+for muscle in cage_conf.muscle_units:
+    attachment_point = muscle['viaPoints'][-1]
+    link_name = attachment_point['link']
+    if link_name not in tendon_groups:
+        tendon_groups[link_name] = {}
+    tendon_groups[link_name][muscle['id']] = {}
+    tendon_groups[link_name][muscle['id']]['point'] = muscle['viaPoints'][0]['point']
+    tendon_groups[link_name][muscle['id']]['max_force'] = muscle['parameters']['max_force']
+
+print(f"\n{len(tendon_groups)} joints with tendons attached found in ({cage_conf_file_path}):")
+for joint in tendon_groups:
+   print(f"\t- {joint}\t: {len(tendon_groups[joint])} tendons {[id for id in tendon_groups[joint]]}")
+print("\n")
+
+# print("tendongroups", tendon_groups['human/left_wrist'][1]['point'])
+# attachment = np.array([])
+# for id in tendon_groups['human/left_wrist']:
+#     attachment = np.append(attachment, tendon_groups['human/left_wrist'][id]['point'])
+#
+# attachment = attachment.reshape(-1, 3)
+
+def get_attachment_points(radius, hight, angles, joint):
+    attachment_points = np.zeros((3, 8))
+    attachment_points[0, 0] = radius
+    attachment_points[0, 1] = radius * np.cos(angles[0])
+    attachment_points[0, 2] = radius * np.cos(angles[1])
+    attachment_points[0, 3] = 0
+    attachment_points[0, 4] = radius
+    attachment_points[0, 5] = radius * np.cos(angles[0])
+    attachment_points[0, 6] = radius * np.cos(angles[1])
+    attachment_points[0, 7] = 0
+    attachment_points[2, 0:3] = hight
+    attachment_points[2, 3] = 1.5
+    attachment_points[2, 4:7] = 0
+    attachment_points[2, 7] = 1
+
+    if joint=="human/left_wrist":
+        attachment_points[1, 0] = 0
+        attachment_points[1, 1] = radius * np.sin(angles[0])
+        attachment_points[1, 2] = radius * np.sin(angles[1])
+        attachment_points[1, 3] = 0
+        attachment_points[1, 4] = 0
+        attachment_points[1, 5] = radius * np.sin(angles[0])
+        attachment_points[1, 6] = radius * np.sin(angles[1])
+        attachment_points[1, 7] = 0
+    elif joint=="human/right_wrist":
+        attachment_points[1, 0] = 0
+        attachment_points[1, 1] = -radius * np.sin(angles[0])
+        attachment_points[1, 2] = -radius * np.sin(angles[1])
+        attachment_points[1, 3] = 0
+        attachment_points[1, 4] = 0
+        attachment_points[1, 5] = -radius * np.sin(angles[0])
+        attachment_points[1, 6] = -radius * np.sin(angles[1])
+        attachment_points[1, 7] = 0
+    return attachment_points
+
+### define workspace ###
+def workspace_desired(stepsize=0.2, joint="human/left_wrist"):
+    x, y, z = np.meshgrid(np.arange(-1, 1 + stepsize, stepsize),
+                          np.arange(-1, 1 + stepsize, stepsize),
+                          np.arange( 0, 2 + stepsize, stepsize))
 
     xx = x.reshape(-1)
     yy = y.reshape(-1)
     zz = z.reshape(-1)
-
-    wx = np.zeros_like(xx)
-    wy = np.zeros_like(yy)
-    wz = np.zeros_like(zz)
-
+    ws_array_desired = np.array([])
+    ws = np.array([xx, yy, zz])
     for i, (mx, my, mz) in enumerate(zip(xx, yy, zz)):
         point = np.array([mx, my, mz])
-        forces = force_decomposition(force_vector, point, tendons)
-        if forces:
-            if force_vector[0] > 0: wx[i] = 1
-            if force_vector[1] > 0: wy[i] = 1
-            if force_vector[2] > 0: wz[i] = 1
+        if joint == "human/left_wrist":
+            if 0.2 <= np.linalg.norm(point - np.array([0, 0, 1])) <= 0.704 and mx >= 0 and my >= 0:
+                ws_array_desired = np.append(ws_array_desired, 1)
+            else:
+                ws_array_desired = np.append(ws_array_desired, 0)
+        elif joint == "human/right_wrist":
+            if 0.2 <= np.linalg.norm(point - np.array([0, 0, 1])) <= 0.704 and mx >= 0 and my <= 0:
+                ws_array_desired = np.append(ws_array_desired, 1)
+            else:
+                ws_array_desired = np.append(ws_array_desired, 0)
 
-    ws = wx + wy + wz
-    ws_size = ws[ws>0].size
-    ws_percentage = np.around(ws_size / x.size, 2)
+    ii = np.where(ws_array_desired == 1)[0]
+    ws_desired = ws[:, ii]
+    return ws_array_desired, ws_desired
 
-    print(f"Done! [{np.around(time.time() - start, 3)} s] available workspace {ws_percentage * 100} %\n")
-    return wx.reshape(x.shape), wy.reshape(y.shape), wz.reshape(z.shape)
+def workspace_feasible(stepsize=0.2, joint="human/left_wrist", radius=1, height=2, angles=np.array([np.pi/3, 2*np.pi/3]), forces=np.array([[100, 0, 0]])):
+    attachment_point = get_attachment_points(radius, height, angles, joint)
+    x, y, z = np.meshgrid(np.arange(-1, 1 + stepsize, stepsize),
+                          np.arange(-1, 1 + stepsize, stepsize),
+                          np.arange( 0, 2 + stepsize, stepsize))
 
+    xx = x.reshape(-1)
+    yy = y.reshape(-1)
+    zz = z.reshape(-1)
+    ws = np.array([xx, yy, zz])
+    ws_array_feasible = np.ones_like(xx)
 
-if __name__ == "__main__":
-    ######   GETTING TENDONS FROM CAGE CONFIGURATION FILE   ######
+    for n, force in enumerate(forces):
+        print(force)
+        for i, (mx, my, mz) in enumerate(zip(xx, yy, zz)):
+            if ws_array_feasible[i] == 1:
+                point = np.array([mx, my, mz])
+                decomposed_forces = force_decomposition(force, point, tendon_groups[joint], attachment_point)
+                if decomposed_forces:
+                    ws_array_feasible[i] = 1
+                else:
+                    ws_array_feasible[i] = 0
 
-    cage_conf_file_path = os.path.dirname(os.path.realpath(__file__)) + "/../config/cageConfiguration.xml"
-    cage_conf = CageConfiguration(cage_conf_file_path)
+    ii = np.where(ws_array_feasible == 1)[0]
+    ws_feasible = ws[:, ii]
+    return ws_array_feasible, ws_feasible
 
-    # print(cage_conf)
-
-    tendon_groups = {}
-    for muscle in cage_conf.muscle_units:
-        attachment_point = muscle['viaPoints'][-1]
-        link_name = attachment_point['link']
-        if link_name not in tendon_groups:
-            tendon_groups[link_name] = {}
-        tendon_groups[link_name][muscle['id']] = {}
-        tendon_groups[link_name][muscle['id']]['point'] = muscle['viaPoints'][0]['point']
-        tendon_groups[link_name][muscle['id']]['max_force'] = muscle['parameters']['max_force']
-
-    print(f"\n{len(tendon_groups)} joints with tendons attached found in ({cage_conf_file_path}):")
-    for joint in tendon_groups:
-        print(f"\t- {joint}\t: {len(tendon_groups[joint])} tendons {[id for id in tendon_groups[joint]]}")
-    print("\n")
-
-
-    #############      APPLY FORCES TEST      #############
-
-    # # force beeing applied to all joints
-    # force = np.array([100, 0, 0])
-
-    # # points where the force is going to be applied
-    # human_attachment_points = [np.array([0, 0, 0]) for _ in tendon_groups]
-
-    # for attachment_point, joint in zip(human_attachment_points, tendon_groups):
-    #     forces = force_decomposition(force, attachment_point, tendon_groups[joint])
-    #     print(f"Decomposed force {force} for joint [{joint}]:")
-    #     for tendon in forces:
-    #         print(f"\t- tendon {tendon}: \t{np.around(forces[tendon],2):>6} Nm\n")
-
-
-    #############     PLOT WORKSPACE     #############
-
-    height = cage_conf.cage_structure['height']
-    radius = cage_conf.cage_structure['radius']
-    stepsize = 0.2
-
-    x, y, z = np.meshgrid(np.arange(-radius, radius, stepsize),
-                        np.arange(-radius, radius, stepsize),
-                        np.arange(0, height, stepsize))
-
-
+# def cost_function(radius, height, angles):
+def cost_function(param):
+    radius = param[0]
+    height = param[1]
+    angles = param[2::]
     forces = np.array([[100, 0, 0],
-                        [0, 100, 0],
-                        [0, 0, 100]])
+                       [0, 100, 0],
+                       [0, 0, 100],
+                       [-100, 0, 0],
+                       [0, -100, 0],
+                       [0, 0, -100]])
+    ws_array_desired, _ = workspace_desired()
+    ws_array_feasible, _ = workspace_feasible(radius=radius, height=height, angles=angles, forces=forces)
+    loss = np.dot((ws_array_desired - ws_array_feasible), (ws_array_desired - ws_array_feasible))
+    print(loss)
+    print(param)
+    return loss
 
 
-    fig = plt.figure(figsize=plt.figaspect(0.3))
-    fig.suptitle('ExoForce workspace', fontsize=16)
-    for i, force in enumerate(forces):
-        ax = fig.add_subplot(1, len(forces), i+1, projection='3d')
-        ax.set(xlim=(-radius, radius),
-               ylim=(-radius, radius),
-               zlim=(0,height))
-        ax.set_title(f"Force = {force}")
-        ax.set_xlabel("X axis")
-        ax.set_ylabel("Y axis")
-        ax.set_zlabel("Height")
+initial_forces = np.array([1, 2, np.pi/3, 2*np.pi/3])
+constraint = [{'type': 'ineq', 'fun': lambda x: x}]
+solution = minimize(cost_function, initial_forces, method='Nelder-Mead', options={'maxiter': 10})
+# solution = minimize(cost_function, initial_forces, constraints=constraint, options={'maxiter': 5})
+print(solution)
+radius = solution.x[0]
 
-        lx, ly, lz = generate_workspace(x, y, z, "human/left_wrist", tendon_groups, force)
-        rx, ry, rz = generate_workspace(x, y, z, "human/right_wrist", tendon_groups, force)
+######################################################################
+######################################################################
+## display workspace under certain conditifullspace & wsdict[i]ons ##
+forces = np.array([[100, 0, 0],
+                   [0, 100, 0],
+                   [0, 0, 100],
+                   [-100, 0, 0],
+                   [0, -100, 0],
+                   [0, 0, -100]])
 
-        ax.quiver(x, y, z, lx, ly, lz, length=0.1, normalize=True, color='b', label="Left Wrist")
-        ax.quiver(x, y, z, rx, ry, rz, length=0.1, normalize=True, color='r', label="Right Wrist")
-        ax.legend()
+ws_array_feasible_L, ws_feasible_L = workspace_feasible(forces=forces)
+ws_array_desired_R, ws_desired_R = workspace_desired(joint="human/right_wrist")
 
-    plt.show()
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(ws_feasible_L[0, :], ws_feasible_L[1, :], ws_feasible_L[2, :], c='c')
+ax.scatter( ws_desired_R[0, :],  ws_desired_R[1, :],  ws_desired_R[2, :], c='r')
+plt.show()
+############################################
+############################################
+# print(ws_desired, ws_feasible, ws_array_desired, ws_array_feasible)
+# print("delta workspace volum", np.sum(ws_array_desired - ws_array_feasible))
+
+# def cost_function(attachment_points):
+#     _, _, ws_array_desired, ws_array_feasible = workspace(side='left', attachment_points=attachment_points)
+#     loss = np.dot((ws_array_desired - ws_array_feasible), (ws_array_desired - ws_array_feasible))
+#     return loss
+
+
+# print(cost_function(1000,2000))
+
+# def cost_function(ws_desired, ws_feasible):
+#     loss = np.dot((ws_desired - ws_feasible), (ws_desired - ws_feasible))
+#     return loss
+
+# constraints = [{'type':'eq', 'fun': lambda x: np.sum(active_tendons.T * np.array([x]), axis=1) - force_vector},
+#                 {'type':'ineq', 'fun': lambda x: active_max_forces - x},
+#                 {'type':'ineq', 'fun': lambda x: x}]
+# solution = minimize(equalize, initial_forces, constraints=constraints, options={'maxiter': 500})

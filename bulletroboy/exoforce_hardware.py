b2f882cb8293ade80793aa3382218447e55616b8
Manuel Lañas <manuel.lanas@outlook.com>
2020-09-04 14:56:43 +0200

    more changes

--- bulletroboy/exoforce_hardware.py
+++ bulletroboy/exoforce_hardware.py
@@ -1,8 +1,10 @@
 import numpy as np
 import time
+from pyquaternion import Quaternion
 
 from .exoforce import ExoForce
 from .utils import load_roboy_to_human_link_name_map
+from .force_decomposition import decompose_force_link_to_ef
 
 from roboy_simulation_msgs.msg import Collision
 from roboy_middleware_msgs.msg import MotorCommand, MotorState
@@ -15,8 +17,9 @@ from rclpy.executors import MultiThreadedExecutor
 from rclpy.callback_groups import ReentrantCallbackGroup
 
 # POS_CONTROL_THRESHOLD = 0.1
-TENSION_PWM_FAST = 500
-TENSION_PWM_SLOW = 200
+FAST_PWM_TENSION = 300
+MIN_PWM_TENSION = 200
+YES_OPTIONS = ["Yes","yes","y",""]
 
 class ExoforceHW(ExoForce):
 	def __init__(self, cage_conf):
@@ -29,63 +32,99 @@ class ExoforceHW(ExoForce):
 		self.link_names_map = load_roboy_to_human_link_name_map()
 		self.callback_group = ReentrantCallbackGroup()
 
-		# self.create_subscription(Collision, '/roboy/simulation/exoforce/operator/collisions', self.collision_listener, 1)
+		self.create_subscription(Collision, '/roboy/simulation/exoforce/operator/collisions', self.collision_listener, 1)
 		# self.create_subscription(PoseStamped, '/roboy/simulation/operator/pose/endeffector', self.ef_pos_listener, 1)
 		self.create_subscription(PoseStamped, '/bullet_ik', self.operator_ef_pos_listener, 10, callback_group=self.callback_group)
 		# self.create_subscription(PoseStamped, '/roboy/simulation/roboy/ef_pose', self.roboy_ef_pos_listener, 10, callback_group=self.callback_group)
-		self.create_subscription(MotorState, '/roboy/middleware/MotorState', self.motor_state_listener, 1, callback_group=self.callback_group)
 		
-		self.motor_command_publisher = self.create_publisher(MotorCommand, '/roboy/middleware/MotorCommand', 1)
+		self.roboy_plexus_ready = False
+		choice = input("Connect to roboy plexus? (Yes/no): ")
+		if choice in YES_OPTIONS:
+			self.create_subscription(MotorState, '/roboy/middleware/MotorState', self.motor_state_listener, 1, callback_group=self.callback_group)
+			self.motor_command_publisher = self.create_publisher(MotorCommand, '/roboy/middleware/MotorCommand', 1)
 
-		self.control_mode_client = self.create_client(ControlMode, '/roboy/middleware/ControlMode')
-		while not self.control_mode_client.wait_for_service(timeout_sec=1.0):
-			self.get_logger().info('roboy_plexus not available, waiting again...')
-		self.get_logger().info('Succesfull connection to roboy plexus!')
+			self.control_mode_client = self.create_client(ControlMode, '/roboy/middleware/ControlMode')
+			while not self.control_mode_client.wait_for_service(timeout_sec=1.0):
+				self.get_logger().info('roboy_plexus not available, waiting again...')
+			self.get_logger().info('Succesfull connection to roboy plexus!')
+			self.roboy_plexus_ready = True
 
-		self.control_mode_req = ControlMode.Request()
-		self.motor_command_msg = MotorCommand()
+			self.control_mode_req = ControlMode.Request()
+			self.motor_command_msg = MotorCommand()
 
-		self.set_control_mode(3)
+			self.set_control_mode(3)
 
-		self.init_tendons()
-		choice = input("\nDo you want to initialize motor position calibration? (Yes/no): ")
-		if choice in ["Yes","yes", "y", ""]:
+			self.init_tendons()
+
+		choice = input("\nCalibrate motor positions? (Yes/no): ")
+		if choice in YES_OPTIONS:
 			self.calibration_timer = self.create_timer(2, self.calibration)
+		else:
+			self.start_exoforce()
+
+		self.applying_force = False
+		self.applying_force_timer = self.create_timer(1, self.stop_applying_force)
+		self.applying_force_timer.cancel()
+
+	def stop_applying_force(self):
+		self.applying_force = False
+
+	def start_exoforce(self):
+		self.create_timer(0.5, self.update)
 
 	def init_tendons(self):
-		choice = input("\nDo you want to initialize the tendons? (Yes/no): ")
-		if choice in ["Yes","yes", "y", ""]:
-			print("\nInitiating tendons:")
-			for muscle in self.muscle_units:
-				tendon = muscle.id
-				print("\n- Tendon "+ str(tendon) + ":")
-				input("Press enter to start...")
-				self.send_motor_commands([tendon], [float(TENSION_PWM_FAST * muscle.direction)])
-				input("Press enter to slowdown...")
-				self.send_motor_commands([tendon], [float(TENSION_PWM_SLOW * muscle.direction)])
-				input("Press enter to stop...")
-				self.send_motor_commands([tendon], [0.0])
+		choice = input("\nInitialize tendons? (Yes/no): ")
+		if choice in YES_OPTIONS:
+			for ef in self.end_effectors:
+				input("\n- Press enter to initialize " + ef + " tendons...")
+				motor_ids = []
+				set_points = []
+				for muscle in self.get_ef_muscle_units(ef):
+					motor_ids.append(muscle.id)
+					set_points.append(float(FAST_PWM_TENSION * muscle.direction))
+				self.send_motor_commands(motor_ids, set_points)
+				input("- Press enter to finish")
+				set_points = []
+				for muscle in self.get_ef_muscle_units(ef):
+					set_points.append(float(MIN_PWM_TENSION * muscle.direction))
+				self.send_motor_commands(motor_ids, set_points)
 			print("\nTendons ready!\n")
+	
+	# def init_tendons(self):
+	# 	choice = input("\nInitialize tendons? (Yes/no): ")
+	# 	if choice in YES_OPTIONS:
+	# 		print("\nInitiating tendons:")
+	# 		for muscle in self.muscle_units:
+	# 			tendon = muscle.id
+	# 			print("\n- Tendon "+ str(tendon) + ":")
+	# 			input("Press enter to start...")
+	# 			self.send_motor_commands([tendon], [float(FAST_PWM_TENSION * muscle.direction)])
+	# 			input("Press enter to slowdown...")
+	# 			self.send_motor_commands([tendon], [float(MIN_PWM_TENSION * muscle.direction)])
+	# 			# input("Press enter to stop...")
+	# 			# self.send_motor_commands([tendon], [0.0])
+	# 		print("\nTendons ready!\n")
 
 	def calibration(self):
+		print(self.end_effectors.values())
 		if np.any([p is None for p in self.end_effectors.values()]):
-			print("Waiting for end effector initial positions...")
+			print("Waiting for end effector initial poses...")
 			return
 
 		print("\nInitializing motor position calibration:")
 		for muscle in self.muscle_units:
 			input("- Touch attachment point " + str(muscle.id) + " with the right hand tracker and press enter...")
-			muscle.motor.via_point.world_point = self.end_effectors["right_wrist"]
+			muscle.motor.via_point.world_point = self.end_effectors["right_wrist"]["position"]
 			print("Motor " + str(muscle.id) + " position updated to: " + str(muscle.motor.via_point.world_point))
 
 		print("\nMotor positions updated!\n")
+		self.start_exoforce()
 		self.calibration_timer.destroy()
 
-		#self.create_timer(0.1, self.print_distance)
 
 	def print_distance(self):
 		for muscle in self.muscle_units:
-			dist = np.linalg.norm(muscle.motor.via_point.world_point - self.end_effectors["right_wrist"])
+			dist = np.linalg.norm(muscle.motor.via_point.world_point - self.end_effectors["right_wrist"]["position"])
 			if dist < 0.1:
 				self.get_logger().info("Motor " + str(muscle.id) + " touched!")
 			if muscle.id == 3:
@@ -114,20 +153,39 @@ class ExoforceHW(ExoForce):
 		
 		"""
 		self.get_logger().info(f"Received collision: link: {collision_msg.linkid} force: {collision_msg.normalforce}")
-		# collision_direction = np.array([collision_msg.contactnormal.x, collision_msg.contactnormal.y, collision_msg.contactnormal.z])
-			
-		# _, rotation = p.getLinkState(self.operator.body_id, collision_msg.linkid)[:2]
-		# rotation = np.array(p.getMatrixFromQuaternion(rotation)).reshape(3,3)
+
+		ef = decompose_force_link_to_ef(collision_msg.linkid)
+		############ TEMP WORKAROUND
+		# for muscle in self.get_ef_muscle_units(ef):
+		# 	muscle.end_effector.world_point = np.array([0.5, -0.5, -0.3])
+		##########################################################################
+		collision_direction = np.array([collision_msg.contactnormal.x, collision_msg.contactnormal.y, collision_msg.contactnormal.z])
+
+		quaternion = Quaternion(self.end_effectors[ef]["orientation"])
+		force_direction = quaternion.rotation_matrix.dot(collision_direction)
+
+		forces = self.decompose(collision_msg.linkid, collision_msg.normalforce, force_direction)
+		print(forces)
 		
-		# force_direction = rotation.dot(collision_direction)
-		# forces = self.decompose(collision_msg.linkid, collision_msg.normalforce, force_direction)
-		
-		# for tendon in self.sim_tendons:
-		# 	if tendon.tendon.id in forces:
-		# 		force = forces[tendon.tendon.id]
-		# 	else:
-		# 		force = 0
-		# 	self.update_tendon(tendon.tendon.id, force)
+		for muscle in forces:
+			self.get_muscle_unit(muscle).set_motor_force(forces[muscle])
+
+		motor_ids = []
+		set_points = []
+		for muscle in self.get_ef_muscle_units(ef):
+			motor_ids.append(muscle.id)
+			if forces[muscle.id] == 0:
+				#set_points.append(float(MIN_PWM_TENSION * muscle.direction * -1))
+				set_points.append(0.0)
+			else:
+				set_points.append(self.get_set_point(forces[muscle.id]) * muscle.direction)
+
+		for id, points in zip(motor_ids, set_points):
+			self.get_logger().info("motor " + str(id) + ": " + str(points))
+
+		self.send_motor_commands(motor_ids, set_points)
+		self.applying_force = True
+		self.applying_force_timer.reset()
 
 	def operator_ef_pos_listener(self, ef_pose):
 		"""Callback of the pose subscriber. Sets the pose of the link given in the msg.
@@ -154,12 +212,15 @@ class ExoforceHW(ExoForce):
 		muscles = self.get_ef_muscle_units(end_effector)
 
 		link_pos = np.array([ef_pose.pose.position.x, ef_pose.pose.position.y, ef_pose.pose.position.z])
+		link_orn = np.array([ef_pose.pose.orientation.x, ef_pose.pose.orientation.y, ef_pose.pose.orientation.z, ef_pose.pose.orientation.w])
 		for muscle in muscles:
 			muscle.end_effector.world_point = link_pos
 
 		if self.end_effectors[end_effector] is None:
-			self.get_logger().info("Got ef initial pos: " + end_effector)
-		self.end_effectors[end_effector] = link_pos
+			self.get_logger().info("Got ef initial pose: " + end_effector)
+			self.end_effectors[end_effector] = {"position": None, "orientation": None}
+		self.end_effectors[end_effector]["position"] = link_pos
+		self.end_effectors[end_effector]["orientation"] = link_orn
 
 	def roboy_ef_pos_listener(self, ef_pose):
 		if ef_pose.header.frame_id in self.link_names_map:
@@ -176,23 +237,27 @@ class ExoforceHW(ExoForce):
 		Returns:
 		   	-
 
-		"""	
-		pass
-
-	def update_tendon(self, id, force):
-		"""Applies a force to a tendon.
-		
-		Args:
-			id (int): Id of the tendon to update.
-			force (float): Force applied to the tendon.
-
-		Returns:
-		   	-
-
 		"""
-		self.get_muscle_unit(id).set_motor_force(force)
+		if self.applying_force:
+			return
+		motor_ids = []
+		set_points = []
+		for muscle in self.get_ef_muscle_units("right_wrist"):
+			motor_ids.append(muscle.id)
+			speed = 0 if muscle.speed is None else muscle.speed
+			if speed * muscle.direction < 0:
+				set_point = 0.0
+			else:
+				set_point = float(MIN_PWM_TENSION * muscle.direction)
+			set_points.append(set_point)
+		self.send_motor_commands(motor_ids, set_points)
+
+	def get_set_point(self, force):
+		return float(force * 20 + 200)
 
 	def set_control_mode(self, mode, motor_ids=None, set_points=[]):
+		if not self.roboy_plexus_ready:
+			return
 		self.control_mode_req.legacy = False
 		self.control_mode_req.control_mode = mode
 		self.control_mode_req.motor_id = motor_ids if motor_ids is not None else [muscle.id for muscle in self.muscle_units]
@@ -201,6 +266,8 @@ class ExoforceHW(ExoForce):
 		self.future = self.control_mode_client.call_async(self.control_mode_req)
 
 	def send_motor_commands(self, motor_ids=None, set_points=None):
+		if not self.roboy_plexus_ready:
+			return
 		if motor_ids is None:
 			motor_ids = [muscle.id for muscle in self.muscle_units]
 		if set_points is None:
@@ -229,6 +296,7 @@ def main(args=None):
 	rclpy.spin(exoforce, executor)
 
 	exoforce.destroy_node()
+	executor.shutdown()
 	rclpy.shutdown()
 
 if __name__ == "__main__":
--- bulletroboy/test_publisher.py
+++ bulletroboy/test_publisher.py
@@ -1,78 +1,117 @@
 
 import rclpy
 import numpy as np
-from roboy_simulation_msgs.msg import TendonUpdate, Collision
-from roboy_control_msgs.msg import CageState, MuscleUnit, ViaPoint
-from geometry_msgs.msg import Point, Vector3
-from std_msgs.msg import Float32
-from bulletroboy.operator import Operator
-import time
+# from roboy_simulation_msgs.msg import TendonUpdate, Collision
+# from roboy_control_msgs.msg import CageState, MuscleUnit, ViaPoint
+# from geometry_msgs.msg import Point, Vector3
+# from std_msgs.msg import Float32
+# from bulletroboy.operator import Operator
+# import time
+from roboy_middleware_msgs.msg import MotorCommand, MotorState
+from roboy_middleware_msgs.srv import ControlMode
 
 def main():
-    rclpy.init()
-    node = rclpy.create_node("test_publisher")
-    #force_publisher = node.create_publisher(TendonUpdate, '/roboy/simulation/tendon_force', 1)
-    #angle_publisher = node.create_publisher(Float32, '/roboy/simulation/cage_rotation', 1)
-    cage_state_publisher = node.create_publisher(CageState, '/roboy/simulation/cage_state', 1)
-    collision_message_publisher = node.create_publisher(Collision, 'roboy/exoforce/collisions', 1)
-    robot_collision_publisher = node.create_publisher(Collision, 'roboy/simulation/roboy/collision', 1)
+	rclpy.init()
+	node = rclpy.create_node("test_publisher")
+	#force_publisher = node.create_publisher(TendonUpdate, '/roboy/simulation/tendon_force', 1)
+	#angle_publisher = node.create_publisher(Float32, '/roboy/simulation/cage_rotation', 1)
+	# cage_state_publisher = node.create_publisher(CageState, '/roboy/simulation/cage_state', 1)
+	# collision_message_publisher = node.create_publisher(Collision, 'roboy/exoforce/collisions', 1)
+	# robot_collision_publisher = node.create_publisher(Collision, 'roboy/simulation/roboy/collision', 1)
 
-    while True:
 
-        # msg = TendonUpdate()
-        # msg.tendon_id = 0
-        # msg.force = 200.0
-        # force_publisher.publish(msg)
+	motor_command_publisher = node.create_publisher(MotorCommand, '/roboy/middleware/MotorCommand', 1)
 
-        # angle = Float32()
-        # angle.data = 58.0
-        # angle_publisher.publish(angle)
+	control_mode_client = node.create_client(ControlMode, '/roboy/middleware/ControlMode')
+	while not control_mode_client.wait_for_service(timeout_sec=1.0):
+		node.get_logger().info('roboy_plexus not available, waiting again...')
+	node.get_logger().info('Succesfull connection to roboy plexus!')
 
-        # state = CageState()
-        # state.rotation_angle = 47.8
-        # for i in range(3):
-        #     muscleunit = MuscleUnit()
-        #     muscleunit.id = i
-        #     for i in range(2):
-        #         point = ViaPoint()
-        #         point.id = i
-        #         point.position = Point()
-        #         point.position.x = 0.0
-        #         point.position.y = 0.0
-        #         point.position.z = 0.0
-        #         point.reference_frame = 'world'
-        #         point.link = 'hand'
-        #         muscleunit.viapoints.append(point)
-        #     state.muscleunits.append(muscleunit)
+	motor = int(input("motor to control: "))
+	set_control_mode(control_mode_client, 3, [motor], [0])
 
-        # cage_state_publisher.publish(state)
+	while True:
+		set_point = float(input("Set point for motor " + str(motor) + ": "))
+		
+		set_point = np.clip(set_point, -1000, 1000)
+		send_motor_commands(motor_command_publisher, [motor], [set_point])
 
-        t = time.time()
-        contact_info = Collision()
-        contact_info.linkid = 8
-        contact_info.position.x = 0.0
-        contact_info.position.y = 0.0
-        contact_info.position.z = 0.0
-        contact_info.contactnormal.x = 0.0
-        contact_info.contactnormal.y = -1.0
-        contact_info.contactnormal.z = 0.0
-        contact_info.normalforce = 5000.0
+		# msg = TendonUpdate()
+		# msg.tendon_id = 0
+		# msg.force = 200.0
+		# force_publisher.publish(msg)
 
-        collision_message_publisher.publish(contact_info)
-        exit()
+		# angle = Float32()
+		# angle.data = 58.0
+		# angle_publisher.publish(angle)
 
-        robotCollision = Collision()
-        robotCollision.linkid = 1
-        robotCollision.position = Vector3()
-        robotCollision.contactnormal = Vector3()
-        robotCollision.contactdistance = float(5)
-        robotCollision.normalforce = float(3)
+		# state = CageState()
+		# state.rotation_angle = 47.8
+		# for i in range(3):
+		#     muscleunit = MuscleUnit()
+		#     muscleunit.id = i
+		#     for i in range(2):
+		#         point = ViaPoint()
+		#         point.id = i
+		#         point.position = Point()
+		#         point.position.x = 0.0
+		#         point.position.y = 0.0
+		#         point.position.z = 0.0
+		#         point.reference_frame = 'world'
+		#         point.link = 'hand'
+		#         muscleunit.viapoints.append(point)
+		#     state.muscleunits.append(muscleunit)
 
-        robot_collision_publisher.publish(robotCollision)
+		# cage_state_publisher.publish(state)
 
-        time.sleep(0.1)
+		# t = time.time()
+		# contact_info = Collision()
+		# contact_info.linkid = 8
+		# contact_info.position.x = 0.0
+		# contact_info.position.y = 0.0
+		# contact_info.position.z = 0.0
+		# contact_info.contactnormal.x = 0.0
+		# contact_info.contactnormal.y = -1.0
+		# contact_info.contactnormal.z = 0.0
+		# contact_info.normalforce = 5000.0
 
+		# collision_message_publisher.publish(contact_info)
+		# exit()
+
+		# robotCollision = Collision()
+		# robotCollision.linkid = 1
+		# robotCollision.position = Vector3()
+		# robotCollision.contactnormal = Vector3()
+		# robotCollision.contactdistance = float(5)
+		# robotCollision.normalforce = float(3)
+
+		# robot_collision_publisher.publish(robotCollision)
+
+		# time.sleep(0.1)
+
+
+def set_control_mode(client, mode, motor_ids, set_points=[]):
+	control_mode_req = ControlMode.Request()
+
+	control_mode_req.legacy = False
+	control_mode_req.control_mode = mode
+	control_mode_req.motor_id = motor_ids
+	control_mode_req.set_points = set_points
+
+	client.call_async(control_mode_req)
+
+def send_motor_commands(publisher, motor_ids, set_points=None):
+	motor_command_msg = MotorCommand()
+
+	if set_points is None:
+		set_points = [0.0] * len(motor_ids)
+
+	motor_command_msg.legacy = False
+	motor_command_msg.motor = motor_ids
+	motor_command_msg.setpoint = set_points
+
+	publisher.publish(motor_command_msg)
 
 if __name__ == '__main__':
-    main()
+	main()
 

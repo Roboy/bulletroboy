3b8632e691b41069309c3924f5659531d49a5ff7
missxa <aliona.kharchenko@gmail.com>
2021-03-01 23:23:46 +0100

    fixes

--- src/ik.py
+++ src/ik.py
@@ -34,7 +34,7 @@ if args.wait:
 
 def quaternion_multiply(quaternion1, quaternion0):
     x0, y0, z0, w0 = quaternion0
-    x1, y1, z1, w1 = quaternion1joint_targets_cb
+    x1, y1, z1, w1 = quaternion1
     return np.array([x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
                      -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0,
                      x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0,
@@ -116,8 +116,8 @@ for i in range(numJoints):
         efs["hand_left"] = i
 
 
-height = 480
-width = 640
+height = 240
+width = 320
 aspect = width/height
 
 fov, nearplane, farplane = 100, 0.1, 100
@@ -295,8 +295,8 @@ def joint_targets_cb(msg):
                                     controlMode=p.POSITION_CONTROL,
                                     targetPosition=msg.position[i],
                                     targetVelocity=0,
-                                    force=100,
-                                    positionGain=1,
+                                    force=40,
+                                    positionGain=0.1,
                                     velocityGain=0.1)
 
 def cmdVelCB(msg):
